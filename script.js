846cbd5595a367de356f46b1a1e996576130f8b8
Francisco-Garrido-DA <116575914+fran-garrido-da@users.noreply.github.com>
2025-05-13 10:57:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const DATAPATH = './DATA.json' 
+
+let menu = await getMenu();
+menu = Array.from(menu);
+const main = document.getElementById("main");
+
+async function getMenu() {
+        try {
+            const res = await fetch(DATAPATH);
+            if (!res.ok) {
+                throw new Error(`HTTP Error. Status: ${res.status}`);
+            }
+            const data = await res.json();
+            return data;
+        } catch (err) {
+            console.error(err);
+            return { error: true, status: err.status, message: err.message };
+        }
+    }
+
+function newCard(menuItem){
+    const newCard = document.createElement("article");
+    newCard.style.width = "30rem";
+    newCard.innerHTML 
+    = `<blockquote>
+        <h1>${menuItem.name}</h5>          
+        <p>Precio: $${menuItem.price}</p>
+        <legend>Descripcion:${menuItem.descrip}</legend>
+        </blockquote>
+        `
+    
+        main.appendChild(newCard);
+
+}
+function showMenu(){
+    menu.forEach(item => {
+        newCard(item);
+        console.log("Added new Card to Main");
+    });
+}
+
+showMenu();
